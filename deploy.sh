f9bdbf4ddccc5f34fdf7be834c604614b67d6d04
Nicolas Bourras <dev@nicolasb.fr>
2020-06-10 01:31:14 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+echo "1. To vivescere/personal-website"
+
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
@@ -15,6 +17,9 @@ hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 git add .
 git commit -m "$msg"
+git push origin master
+
+echo "2. To vivescere.github.io"
 
 # Go To Public folder
 cd public
